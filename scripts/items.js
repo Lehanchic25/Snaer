c42e1740e12a090cc662e900d728bc45cd0b5b95
AnDashik (Andrey Kozlov) <mr.andrekozlov@gmail.com>
2024-11-25 22:21:23 +0300

    Add files via upload

--- scripts/items.js
+++ scripts/items.js
@@ -3,11 +3,11 @@ const lithium = extend(Item, "lithium", {});
 const brownCoal = extend(Item, "brown-coal", {});
 const snaerSilicon = extend(Item, "snaer-silicon", {});
 const palladium = extend(Item, "palladium", {});
-const phosphorus = extend(Item, "phosphorus", {});
 const snaerTungsten = extend(Item, "snaer-tungsten", {});
+const snaerCarbide = extend(Item, "snaer-carbide", {});
+const phosphorus = extend(Item, "phosphorus", {});
 const amorphousAlloy = extend(Item, "amorphous-alloy", {});
 const fluomoss = extend(Item, "fluomoss", {});
-const snaerCarbide = extend(Item, "snaer-carbide", {});
 
 module.exports = {
 rhenium: rhenium,
@@ -15,9 +15,9 @@ lithium: lithium,
 brownCoal: brownCoal,
 snaerSilicon: snaerSilicon,
 palladium: palladium,
-phosphorus: phosphorus,
 snaerTungsten: snaerTungsten,
+snaerCarbide: snaerCarbide,
+phosphorus: phosphorus,
 amorphousAlloy: amorphousAlloy,
-fluomoss: fluomoss,
-snaerCarbide: snaerCarbide
+fluomoss: fluomoss
 }
--- scripts/liquids.js
+++ scripts/liquids.js
@@ -1,15 +1,15 @@
 const coldWater = extend(Liquid, "cold-water", {});
 const methane = extend(Liquid, "methane", {});
-const hydroxide = extend(Liquid, "hydroxide", {});
 const snaerHydrogen = extend(Liquid, "snaer-hydrogen", {});
+const hydroxide = extend(Liquid, "hydroxide", {});
 const fluorine = extend(Liquid, "fluorine", {});
 const hydrofluoricAcid = extend(Liquid, "hydrofluoric-acid", {});
 
 module.exports = {
 coldWater: coldWater,
 methane: methane,
-hydroxide: hydroxide,
 snaerHydrogen: snaerHydrogen,
+hydroxide: hydroxide,
 fluorine: fluorine,
 hydrofluoricAcid: hydrofluoricAcid
 }
